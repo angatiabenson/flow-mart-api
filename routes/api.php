4f9c56401a0ba4fd64ab08006cfee3b75c6a5a17
Angatia Benson <bensonetia@gmail.com>
2024-09-04 11:23:42 -0500

    "Replaced '/user' API route with '/me' route returning JSON response with name and phone number.

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\Api\WebhookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/me', function (Request $request) {
+    return response()->json([
+        "name" => "Peshy",
+        "phone" => "071234567890"
+    ], 200);
 });
 
 
