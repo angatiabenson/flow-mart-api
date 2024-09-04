c5d1b17e4b5e712edd85186cb741ce264fcca8bd
Angatia Benson <bensonetia@gmail.com>
2024-09-04 15:01:42 -0500

    Added unique constraint to 'name' columns in 'categories' and 'products' tables.

--- database/migrations/2024_09_04_181011_create_products_table.php
+++ database/migrations/2024_09_04_181011_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('quantity');
             $table->timestamps();
         });
