548a478914c5365335c22852915a812e01631498
Angatia Benson <bensonetia@gmail.com>
2024-09-04 14:23:54 -0500

    Removed hardcoded /me endpoint and added /register and /login endpoints using UserController

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\WebhookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/me', function (Request $request) {
-    return response()->json([
-        "name" => "Peshy",
-        "phone" => "071234567890"
-    ], 200);
-});
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
 
 
 Route::group(
