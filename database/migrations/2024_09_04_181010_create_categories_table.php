156c716f9a5a9bf54f1ef0c72f32eba4b1d96b00
Angatia Benson <bensonetia@gmail.com>
2024-09-05 02:53:41 -0500

    Removed unique constraint from 'name' columns in 'categories' and 'products' tables.

--- database/migrations/2024_09_04_181010_create_categories_table.php
+++ database/migrations/2024_09_04_181010_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Foreign Key
-            $table->string('name')->unique();
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/migrations/2024_09_04_181011_create_products_table.php
+++ database/migrations/2024_09_04_181011_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->string('quantity');
             $table->timestamps();
         });
